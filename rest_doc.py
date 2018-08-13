7f5ef37c501cd1fc3d0fe7f244d3e3536d190fc4
luca <luca.battistella.geo@gmail.com>
2018-08-13 11:58:10 +0200

    Update rest_doc.py

--- rest_doc.py
+++ rest_doc.py
@@ -20,8 +20,8 @@ print """Content-type: text/html
 </dl>
 <hr></hr>
 
-<pre>Call example: <h1><a href='http://localhost:8888/rest.py?type=fun&schema=public&obj=f_wdpa_area&params=(wdpaid:921)' target='_blank'>Link</a></h1></pre>
-eg: "http://localhost:8888/rest.py?type=fun&schema=public&obj=f_wdpa_area&params=(wdpaid:921)"
+<pre>Call example: <h1><a href='http://localhost:8888/rest.py?type=fun&schema=public&obj=f_wdpa_lcc&params=(wdpaid:921)' target='_blank'>Link</a></h1></pre>
+eg: "http://localhost:8888/rest.py?type=fun&schema=public&obj=f_wdpa_lcc&params=(wdpaid:921)"
 
 <p>where </p> 
 <p><b>type=</b> can be 'tab' (for tables and views calls) or 'fun' (to call functions)</p> 
